c1da614a8eefca149d2d5c0d1ff34f61c69620c5
ncabanes <ncabanes@ba89d313-b212-416f-aaae-7e614c33bc22>
2012-11-18 00:31:15 +0000

    Version H: collisions with background

--- trunk/game.cs
+++ trunk/game.cs
@@ -1,31 +1,31 @@
 /*
   First mini-graphics-game skeleton
-  Version G: dots from map
+  Version H: collisions with background
 */
- 
+
 using System;
- 
+
 public class Game02f
 {
     public static void Main()
     {
         bool fullScreen = false;
         SdlHardware.Init(800, 600, 24, fullScreen);
-        
+
         Image dotImage = new Image("dot.bmp");
         Image enemyImage = new Image("ghostGreen.bmp");
         Image pacImage = new Image("pac01r.bmp");
         Image wallImage = new Image("wall.bmp");
-        
-        int x=32, y=32;
-        int pacSpeed = 6;
-        
+
+        int x = 32, y = 32;
+        int pacSpeed = 4;
+
         int amountOfEnemies = 4;
         float[] xEnemy = { 150, 400, 500, 600 };
         float[] yEnemy = { 100, 200, 300, 400 };
         float[] incrXEnemy = { 5f, 3f, 6f, 4.5f };
-                
-        string [] map = {
+
+        string[] map = {
             "-----------------",
             "-.......-.......-",
             "-.--.--.-.--.--.-",
@@ -56,7 +56,7 @@ public class Game02f
         }
         int[] xDot = new int[amountOfDots];
         int[] yDot = new int[amountOfDots];
-        bool[] visible = new bool[amountOfDots];        
+        bool[] visible = new bool[amountOfDots];
         // Now, assign their coordinates
         int currentDot = 0;
         for (int row = 0; row < 15; row++)
@@ -65,7 +65,7 @@ public class Game02f
             {
                 if (map[row][column] == '.')
                 {
-                    xDot[currentDot] = column*32;
+                    xDot[currentDot] = column * 32;
                     yDot[currentDot] = row * 32;
                     visible[currentDot] = true;
                     currentDot++;
@@ -76,68 +76,151 @@ public class Game02f
 
         int score = 0;
         bool gameFinished = false;
- 
+
         // Game Loop
-        while( ! gameFinished )
+        while (!gameFinished)
         {
             // Draw
             SdlHardware.ClearScreen();
             //Console.Write("Score: {0}",score);
-            
+
             // Background map
-            for(int row=0; row<15; row++)
+            for (int row = 0; row < 15; row++)
             {
-                for(int column=0; column<17; column++)
+                for (int column = 0; column < 17; column++)
                 {
                     if (map[row][column] == '-')
-                        SdlHardware.DrawHiddenImage(wallImage, column*32, row*32);
+                        SdlHardware.DrawHiddenImage(wallImage, column * 32, row * 32);
                 }
             }
-            
-            for(int i=0; i<amountOfDots; i++)
+
+            for (int i = 0; i < amountOfDots; i++)
             {
                 if (visible[i])
                     SdlHardware.DrawHiddenImage(dotImage, xDot[i], yDot[i]);
             }
-            
+
             SdlHardware.DrawHiddenImage(pacImage, x, y);
-            
-            for(int i=0; i<amountOfEnemies; i++)
-                SdlHardware.DrawHiddenImage(enemyImage, 
+
+            for (int i = 0; i < amountOfEnemies; i++)
+                SdlHardware.DrawHiddenImage(enemyImage,
                     (int)xEnemy[i], (int)yEnemy[i]);
-                    
+
             SdlHardware.ShowHiddenScreen();
 
             // Read keys and calculate new position
-            if (SdlHardware.KeyPressed(SdlHardware.KEY_RIGHT)) x += pacSpeed;
-            if (SdlHardware.KeyPressed(SdlHardware.KEY_LEFT)) x -= pacSpeed;
-            if (SdlHardware.KeyPressed(SdlHardware.KEY_DOWN)) y += pacSpeed;
-            if (SdlHardware.KeyPressed(SdlHardware.KEY_UP)) y -= pacSpeed;
-            
-            if (SdlHardware.KeyPressed(SdlHardware.KEY_ESC)) 
+            if (SdlHardware.KeyPressed(SdlHardware.KEY_RIGHT))
+            {
+                bool canMove = true;
+                for (int row = 0; row < 15; row++)
+                {
+                    for (int column = 0; column < 17; column++)
+                    {
+                        if (map[row][column] == '-')
+                            if ((x + pacSpeed > column * 32 - 32) &&
+                                (x + pacSpeed < column * 32 + 32) &&
+                                (y > row * 32 - 32) &&
+                                (y < row * 32 + 32)
+                                )
+                            {
+                                canMove = false;
+                            }
+                    }
+                }
+                if (canMove)
+                    x += pacSpeed;
+            }
+
+            if (SdlHardware.KeyPressed(SdlHardware.KEY_LEFT))
+            {
+                bool canMove = true;
+                for (int row = 0; row < 15; row++)
+                {
+                    for (int column = 0; column < 17; column++)
+                    {
+                        if (map[row][column] == '-')
+                            if ((x - pacSpeed > column * 32 - 32) &&
+                                (x - pacSpeed < column * 32 + 32) &&
+                                (y > row * 32 - 32) &&
+                                (y < row * 32 + 32)
+                                )
+                            {
+                                canMove = false;
+                            }
+                    }
+                }
+                if (canMove)
+                    x -= pacSpeed;
+            }
+
+            if (SdlHardware.KeyPressed(SdlHardware.KEY_DOWN))
+            {
+                bool canMove = true;
+                for (int row = 0; row < 15; row++)
+                {
+                    for (int column = 0; column < 17; column++)
+                    {
+                        if (map[row][column] == '-')
+                            if ((x > column * 32 - 32) &&
+                                (x < column * 32 + 32) &&
+                                (y + pacSpeed > row * 32 - 32) &&
+                                (y + pacSpeed < row * 32 + 32)
+                                )
+                            {
+                                canMove = false;
+                            }
+                    }
+                }
+                if (canMove)
+                    y += pacSpeed;
+            }
+
+            if (SdlHardware.KeyPressed(SdlHardware.KEY_UP))
+            {
+                bool canMove = true;
+                for (int row = 0; row < 15; row++)
+                {
+                    for (int column = 0; column < 17; column++)
+                    {
+                        if (map[row][column] == '-')
+                            if ((x > column * 32 - 32) &&
+                                (x < column * 32 + 32) &&
+                                (y - pacSpeed > row * 32 - 32) &&
+                                (y - pacSpeed < row * 32 + 32)
+                                )
+                            {
+                                canMove = false;
+                            }
+                    }
+                }
+                if (canMove)
+                    y -= pacSpeed;
+            }
+
+            if (SdlHardware.KeyPressed(SdlHardware.KEY_ESC))
                 gameFinished = true;
-                        
+
             // Move enemies and environment
-            for(int i=0; i<amountOfEnemies; i++)
+            for (int i = 0; i < amountOfEnemies; i++)
             {
                 xEnemy[i] += incrXEnemy[i];
                 if ((xEnemy[i] < 1) || (xEnemy[i] > 760))
                     incrXEnemy[i] = -incrXEnemy[i];
             }
- 
+
             // Collisions, lose energy or lives, etc
-            for(int i=0; i<amountOfDots; i++)
-                if ( visible[i] &&
-                    (x > xDot[i] - 32) && 
+            for (int i = 0; i < amountOfDots; i++)
+                if (visible[i] &&
+                    (x > xDot[i] - 32) &&
                     (x < xDot[i] + 32) &&
-                    (y > yDot[i] - 32) && 
+                    (y > yDot[i] - 32) &&
                     (y < yDot[i] + 32)
                     )
                 {
                     score += 10;
                     visible[i] = false;
                 }
-            
+
             // Pause till next fotogram
             SdlHardware.Pause(40);
         }
