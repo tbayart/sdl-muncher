9eeb7013bb0d55a5482c3af5ce89f541e22fbcb6
ncabanes <ncabanes@ba89d313-b212-416f-aaae-7e614c33bc22>
2013-01-25 20:13:20 +0000

    Version OOP.004 - Basic collisions and animations; level with walls and dots

--- trunk/objectOrientedVersion/Enemy.cs
+++ trunk/objectOrientedVersion/Enemy.cs
@@ -8,6 +8,9 @@
  * 0.01, 21-dec-2012: Empty skeleton
  * 0.02, 21-dec-2012: Constructor loads image and places ghost
  * 0.03, 18-ene-2013: Can move side to side
+ * 0.04, 25-ene-2013
+ *     Animated when moving (two images)
+ *     Image changes after n passes (10 as starting value)
  * 
  */
 
@@ -15,18 +18,31 @@ namespace Game
 {
     class Enemy : Sprite
     {
+        protected int maxFramesToWait;  // Game frames needed to change image
+        protected int frameWaitCounter; // Counter
+
         public Enemy()
         {
-            LoadImage("data/ghostGreen.png");
+            LoadSequence(new string[] { "data/ghostGreen.png", "data/ghostGreen2.png" });
             MoveTo(300, 100);
             xSpeed = 2;
+            maxFramesToWait = 10;
+            frameWaitCounter = 0;
         }
 
-        public virtual void Move()
+        public override void Move()
         {
+            // Adjust position and speed
             x += xSpeed;
             if ((x > 700) || (x < 10))
                 xSpeed = -xSpeed;
+            // And change image if needed
+            frameWaitCounter++;
+            if (frameWaitCounter >= maxFramesToWait)
+            {
+                NextFrame();
+                frameWaitCounter = 0;
+            }
         }
 
     }
--- trunk/objectOrientedVersion/Level.cs
+++ trunk/objectOrientedVersion/Level.cs
@@ -7,22 +7,56 @@
  * Changes:
  * 0.03, 18-jan-2013
  *     Skeleton: only draws a block
+ * 0.04, 25-jan-2013
+ *     Draws several blocks, taken from an array
+ *     Dots are also drawn
  */
 
 namespace Game
 {
     class Level
     {
-        Image rock;
+        static string[] map = 
+        {
+            "-----------------",
+            "-.......-.......-",
+            "-.--.--.-.--.--.-",
+            "-...............-",
+            "-.--.-.---.-.--.-",
+            "-....-..-..-....-",
+            "----.--...--.----",
+            ".................",
+            "----.-.-.-.-.----",
+            "-....-.-.-.-....-",
+            "-.--.-.---.-.--.-",
+            "-...............-",
+            "-.--.-.---.-.--.-",
+            "-....-.....-....-",
+            "-----------------"
+        };
+
+        Image rockImage;
+        Image dotImage;
 
         public Level()
         {
-            rock = new Image("data/wall.png");
+            rockImage = new Image("data/wall.png");
+            dotImage = new Image("data/dot.png");
         }
 
         public void DrawOnHiddenScreen()
         {
-            Hardware.DrawHiddenImage(rock, 10, 10);
+            // Background map
+            for (int row = 0; row < 15; row++)
+            {
+                for (int column = 0; column < 17; column++)
+                {
+                    if (map[row][column] == '-')
+                        Hardware.DrawHiddenImage(rockImage, column * 32, row * 32);
+                    if (map[row][column] == '.')
+                        Hardware.DrawHiddenImage(dotImage, column * 32, row * 32);
+                }
+            }
         }
     }
 }
