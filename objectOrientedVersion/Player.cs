4670402e95ed8bf028ead52650f6fff9ce6b6c54
ncabanes <ncabanes@ba89d313-b212-416f-aaae-7e614c33bc22>
2013-02-05 10:31:45 +0000

    Version OOP.007 - Multiple images in multiple directions

--- objectOrientedVersion/Player.cs
+++ objectOrientedVersion/Player.cs
@@ -9,7 +9,9 @@
  * 0.02, 21-dec-2012: Can move to the right
  * 0.03, 18-ene-2013: Can move to the left, up and down
  * 0.04, 25-ene-2013: Animated when moving (two images)
- * 
+ * 0.07, 05-feb-2013: 
+ *   Two images in each direction
+ *   MoveRight and so on can deb used to change the direction
  */
 
 namespace Game
@@ -18,7 +20,10 @@ namespace Game
     {
         public Player()
         {
-            LoadSequence(new string[] { "data/pac01r.png", "data/pac02r.png" });
+            LoadSequence(RIGHT, new string[] { "data/pac01r.png", "data/pac02r.png" });
+            LoadSequence(LEFT, new string[] { "data/pac01l.png", "data/pac02l.png" });
+            LoadSequence(UP, new string[] { "data/pac01u.png", "data/pac02u.png" });
+            LoadSequence(DOWN, new string[] { "data/pac01d.png", "data/pac02d.png" });
             MoveTo(200, 200);
             xSpeed = 4;
             ySpeed = 4;
@@ -26,24 +31,28 @@ namespace Game
 
         public void MoveRight()
         {
+            ChangeDirection(RIGHT);
             x += xSpeed;
             NextFrame();
         }
 
         public void MoveLeft()
         {
+            ChangeDirection(LEFT);
             x -= xSpeed;
             NextFrame();
         }
 
         public void MoveUp()
         {
+            ChangeDirection(UP);
             y -= ySpeed;
             NextFrame();
         }
 
         public void MoveDown()
         {
+            ChangeDirection(DOWN);
             y += ySpeed;
             NextFrame();
         }
