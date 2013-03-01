35d076281a456add1b2360136ea7a838534bf517
ncabanes <ncabanes@ba89d313-b212-416f-aaae-7e614c33bc22>
2013-03-01 16:51:58 +0000

    Basic support for scrolling

--- trunk/objectOrientedVersion/Credits.cs
+++ trunk/objectOrientedVersion/Credits.cs
@@ -8,6 +8,7 @@
  * Changes:
  * 0.01, 21-dec-2012: Empty skeleton
  * 0.02, 21-dec-2012: Basic credits screen, showing static text
+ * 0.10, 01-mar-2013: Scrolling text
  * 
  */
 
@@ -24,21 +25,26 @@ namespace Game
             Font sans18;
             sans18 = new Font("data/Joystix.ttf", 18);
 
-            Hardware.ClearScreen();
-            Hardware.WriteHiddenText("By DAM Ies San Vicente 2012-2013",
-                200, 500,
-                0xCC, 0xCC, 0xCC,
-                sans18);
-            Hardware.WriteHiddenText("Hit ESC to return",
-                300, 540,
-                0x99, 0x99, 0x99,
-                sans18);
-            Hardware.ShowHiddenScreen();
             do
             {
+                Hardware.ClearScreen();
+                Hardware.WriteHiddenText("By DAM Ies San Vicente 2012-2013",
+                    200, 500,
+                    0xCC, 0xCC, 0xCC,
+                    sans18);
+                Hardware.WriteHiddenText("Hit ESC to return",
+                    300, 540,
+                    0x99, 0x99, 0x99,
+                    sans18);
+                Hardware.ShowHiddenScreen();
+
+                Hardware.ScrollVertically(-2);
+
                 Hardware.Pause(20);
             }
             while (!Hardware.KeyPressed(Hardware.KEY_ESC));
+
+            Hardware.ResetScroll();
         }
     }
 }
